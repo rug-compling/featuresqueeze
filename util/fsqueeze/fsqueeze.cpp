23173d8406422bb59edbc1f57bd892bbbc1afadc
Yan Zhao <yzhao@vingolf.let.rug.nl>
2009-09-15 14:50:44 +0200

    The ifstream constructor takes a C string, rather than std::string.

--- util/fsqueeze/fsqueeze.cpp
+++ util/fsqueeze/fsqueeze.cpp
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
 	if (programOptions.option('n'))
 		nFeatures = fsqueeze::parseString<size_t>(programOptions.optionValue('n'));
 
-	ifstream dataStream(programOptions.arguments()[0]);
+	ifstream dataStream(programOptions.arguments()[0].c_str());
 	if (!dataStream)
 	{
 		cerr << "Error opening input file!" << endl;
